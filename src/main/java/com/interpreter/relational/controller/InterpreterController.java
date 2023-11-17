5190443408e058799ef21ec6cd1b147266937241
Galiatkin Dmitriy <dimadim202@gmail.com>
2023-11-17 19:35:01 +0700

    deleted useless comment with query example

--- src/main/java/com/interpreter/relational/controller/InterpreterController.java
+++ src/main/java/com/interpreter/relational/controller/InterpreterController.java
@@ -21,14 +21,6 @@ public class InterpreterController {
 
     @PostMapping(value = "/execute")
     public ResponseEntity<ResponseDataDto> execute(@RequestBody RequestTestDataDto dto) {
-//        List<String> testQuery = Arrays.asList(
-//                "SELECT R2 WHERE ( NOT phone = 135121 OR username = \"andrew\" ) AND NOT username = \"jim\" -> T1",
-//                "DIFFERENCE R1 AND T1 -> T2",
-//                "DIVIDE R3 BY T2 OVER group username -> T3",
-//                "JOIN T2 AND T3 OVER username -> T4",
-//                "TIMES R3 AND T4 -> T5",
-//                "JOIN T5 AND T4 OVER group username phone"
-//        );
 
         return ResponseEntity
                 .status(HttpStatus.OK)
